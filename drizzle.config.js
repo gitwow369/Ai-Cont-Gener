0a7057a5f6889dbc2efba29bee649142ae4d4bb6
Yord <Yordwow@gmail.com>
2024-09-22 12:33:19 -0700

    AI CONT Finished

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+  schema: "./utils/schema.tsx",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://Named%20Tut%20Ai%20Cont%20YT_owner:4g2VZeANPUnE@ep-gentle-voice-a4zsnaqb.us-east-1.aws.neon.tech/Named%20Tut%20Ai%20Cont%20YT?sslmode=require",
+  },
+};
